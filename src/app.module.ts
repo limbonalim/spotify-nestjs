d6891cf09f773a922461f3767edb946e0c201a72
Zhuravlev Petr <zeterfrost@gmail.com>
2024-03-20 14:13:42 +0600

    created config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArtistController } from './artist/artist.controller';
+import { Artist, ArtistSchema } from './schemas/artist.schema';
+import { Album, AlbumSchema } from './schemas/album.schema';
+import { Track, TrackSchema } from './schemas/track.schema';
+import config from '../config';
+import { ArtistService } from './artist/artist.service';
+import { AlbumController } from './album/album.controller';
+import { AlbumService } from './album/album.service';
+import { TrackController } from './track/track.controller';
+import { TrackService } from './track/track.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(config.mongoose.db),
+    MongooseModule.forFeature([
+      { name: Artist.name, schema: ArtistSchema },
+      { name: Album.name, schema: AlbumSchema },
+      { name: Track.name, schema: TrackSchema },
+    ]),
+  ],
+  controllers: [
+    AppController,
+    ArtistController,
+    AlbumController,
+    TrackController,
+  ],
+  providers: [AppService, ArtistService, AlbumService, TrackService],
 })
 export class AppModule {}
