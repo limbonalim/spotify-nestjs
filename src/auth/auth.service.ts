471b53529ffd2ed378c795886e5061793d2ea9d8
Zhuravlev Petr <zeterfrost@gmail.com>
2024-03-22 20:35:46 +0600

    added authorization

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from 'src/schemas/user.schema';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class AuthService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userModel.findOne({ email: email });
+
+    if (user) {
+      if (await user.checkPassword(password)) {
+        user.generateToken();
+        await user.save();
+        return user;
+      }
+    }
+    return null;
+  }
+}
